3bc2335f3fd32025c341933dc153331341580625
Mrosa90 <145835544+Mrosa90@users.noreply.github.com>
2024-02-12 12:17:06 +0000

    proyecto acabado

--- src/app.js
+++ src/app.js
@@ -1,28 +1,19 @@
 /* eslint-disable */
 import "./style.css";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".es"];
+
 window.onload = () => {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-
-  function generarNombreDominio() {
-    let pronounAleatorio = pronoun[Math.floor(Math.random() * pronoun.length)];
-    let adjAleatorio = adj[Math.floor(Math.random() * adj.length)];
-    let nounAleatorio = noun[Math.floor(Math.random() * noun.length)];
-
-    return `${pronounAleatorio}${adjAleatorio}${nounAleatorio}.com`;
-  }
-
-  function mostrarNombresDominio() {
-    let resultados = [];
-    for (let i = 0; i < 9; i++) {
-      resultados.push(generarNombreDominio());
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let m = 0; m < noun.length; m++) {
+        for (let t = 0; t < extension.length; t++) {
+          console.log(pronoun[i] + adj[j] + noun[m] + extension[t]);
+        }
+      }
     }
-    return resultados;
   }
-
-  document.getElementById(
-    "resultado"
-  ).innerHTML = `<br>${mostrarNombresDominio().join("<br>")}`;
 };
